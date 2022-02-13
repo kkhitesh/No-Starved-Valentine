e6b0db62e3bbb2e808701e9bd0cb9c9e7c6b48c6
sonichigo <kurosakiichigo.songoku@gmail.com>
2022-02-13 15:38:50 +0530

    :gear: Orders Ready

---
+++ frontend/src/Food.js
@@ -0,0 +1,51 @@
+import { Card, Image, Text, Badge, Button, Group, useMantineTheme } from '@mantine/core';
+import React, {useState} from "react";
+import "./styles/Food.css"
+
+function Food( { img, name}) {
+  const theme = useMantineTheme();
+  const [items, setItems] = useState([])
+  const [query, setQuery] = useState("")
+  const onClick = () => {
+  items.concat(query)
+  }
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => {
+    if(count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <div style={{ width: 200, margin: 30 }}>
+      <Card shadow="sm" padding="lg">
+        <Card.Section>
+          <Image src={img} height={100} alt="Norway" />
+        </Card.Section>
+
+        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+          <Text weight={500}>{name}</Text>
+          <Badge color="pink" variant="light">
+            On Sale
+          </Badge>
+        </Group>
+        <div className="divider">
+          <Button size="xs" onClick={decrement}>
+            - 
+          </Button>
+          {count}
+          <Button size="xs" onClick={increment}>
+            +
+          </Button>
+        </div>
+      </Card>
+      <div>{items.map(items =>
+          <div>{items}</div>
+        )}
+        </div>
+    </div>
+  );
+}
+export default Food;
--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
 import { axiosInstance } from "./axios";
+import Button from '@material-ui/core/Button';
+import { Input } from '@mantine/core';
 
-const Form = () => {
+const Form = ({setOpened}) => {
   const [name, setName] = useState("");
   const [num, setNum] = useState("");
-  const [order] = useState(["pizza", "burger", "fries"]);
+  const [adr, setAdr] = useState("");
+  const [order] = useState(["Burger", "Fries", "Coke"]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    alert("Thanks for Ordering 😊");
     const details = { name, num, order };
     axiosInstance.post("/send", details).then((res) => {
       console.log(res);
     });
+    setOpened(false);
   };
 
   return (
-    <div className="form">
-      <h1>Form</h1>
+    <div className="App">
+      <div className="BG">
+      <h2>Order  </h2>
+      &nbsp;&nbsp;&nbsp;&nbsp;
       <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
+        <Input
+          placeholder="Enter Name"
+          value = {name}
+          onChange = {(e) => setName(e.target.value)}
         />
         <br />
-        <input
-          type="text"
-          placeholder="Number"
-          value={num}
-          onChange={(e) => setNum(e.target.value)}
-          required
+        <Input
+          placeholder="Enter Number"
+          value = {num}
+          onChange = {(e) => setNum(e.target.value)}
         />
         <br />
-        <input type="submit" value="Submit" />
+        <Input
+          placeholder="Enter Address"
+          value = {adr}
+          onChange = {(e) => setAdr(e.target.value)}
+        />
+        <br />
+        <Button variant="contained" color="primary" onClick="Submit" >Submit</Button>
       </form>
+    </div>
     </div>
   );
 };
